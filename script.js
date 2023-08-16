a27573e50c357c8c04a937068dae19c7d931b565
victor <briere.v@gmail.com>
2023-08-15 18:17:00 -0700

    added 3rd msg(with switch)

--- script.js
+++ script.js
@@ -43,3 +43,75 @@ const creatMsg = obj => {
 console.log(creatMsg(welcomeNote));
 console.log(messageSelector())
 
+//other examples 
+
+const randomNum = (num)=>{
+    return Math.floor(Math.random()*num)
+}
+
+const statementOne = ()=>{
+    let x = randomNum(4)
+    switch (x){
+        case 0:
+            return "You are a "
+            break;
+        case 1:
+            return "You look like a "
+            break;
+        case 2:
+            return "You smell like a "
+            break;
+        case 3:
+            return "You remind me of a "
+            break;
+        default:
+            "error message"
+            break;        
+    }
+}
+ 
+const statementTwo = ()=>{
+    let x = randomNum(6)
+    switch (x){
+        case 0:
+            return "banana"
+            break;
+        case 1:
+            return "loser"
+            break;
+        case 2:
+            return "armpit"
+            break;
+        case 3:
+            return "princess"
+            break;
+        case 4:
+            return "lemon"
+            break;
+        case 5:
+            return "poo"
+            break;    
+        default:
+            "error message"
+            break;        
+    }
+}
+
+const statementThree = ()=>{
+    let x = randomNum(3)
+    switch (x){
+        case 0:
+            return " on holiday."
+            break;
+        case 1:
+            return " falling asleep."
+            break;
+        case 2:
+            return " up a tree."
+            break;
+        default:
+            return "error message"
+            break;        
+    }
+}
+console.log(statementOne()+statementTwo()+statementThree())
